b06351c78ca76533c81dc854d02c152617b76708
4skl <medi.olivier@hotmail.fr>
2020-08-09 04:10:11 +0200

    Cleaning, correcting, improving
    corrected ersave, added saves at regular intervals, added reactions, moved !faq show <name> to !faq <name>, removed token from the code

--- FAQBot.py
+++ FAQBot.py
@@ -1,12 +1,8 @@
-import os
-
 import discord
 from random import choice
-import json, atexit
+import json, atexit, threading, time
 
-TOKEN = 'TOKEN'
-
-client = discord.Client()
+TOKEN = open('token','r').read()
 
 faq_prefix = '!faq'
 
@@ -18,40 +14,58 @@ message_faq_not_created = 'No FAQ for this server yet'
 message_faq_empty = 'The FAQ of this server is actually empty'
 message_faq_not_found = 'No FAQ with this name found, use `'+faq_prefix+' list` to see all FAQ\'s'
 message_invalid_command = 'Invalid command, see `'+faq_prefix+'` for help'
-message_help = '`'+faq_prefix+'` to see this help\n`'+faq_prefix+' add <name> <description>`\n`'+faq_prefix+' delete <name>`\n`'+faq_prefix+' list`\n`'+faq_prefix+' show <name>`'
+message_help = '`'+faq_prefix+'` to see this help\n`'+faq_prefix+' add <name> <description>`  to add an faq entry\n`'+faq_prefix+' delete <name>` to delete an faq entry\n`'+faq_prefix+' list` to list all faq of the server\n`'+faq_prefix+' <name>` to show an faq'
 
 data_filepath = './datas.json'
 
+data_file = open(data_filepath, 'r')
 try:
-    data_file = open(data_filepath, 'r')
-    try:
-        data = json.load(data_file)
-    except json.JSONDecodeError:
-        open(data_filepath+str(time.time())+'.ersave','w+', encoding="utf-8").write(data_file.read())
-        data = {}
-except FileNotFoundError:
-    data_file = open(data_filepath, 'w+')
+    data = json.load(data_file)
+except json.JSONDecodeError:
+    ersave = open(data_filepath+str(time.time())+'.ersave','w+', encoding="utf-8") # to save erroned datas
+    data_file.seek(0)
+    ersave.write(data_file.read())
+    ersave.close()
     data = {}
+data_file.close()
 
-def on_dest_save():
+## saving
+def save():
     data_file = open(data_filepath, 'w+')
     json.dump(data, data_file)
-    data_file.flush()
     data_file.close()
 
-atexit.register(on_dest_save)
+# add data saving at regular intervals
+def save_at_interval(interval):
+    global save_interval_running
+    while save_interval_running:
+        save()
+        time.sleep(interval)
+
+save_interval_running = True
+save_thread = threading.Thread(target=save_at_interval, args=(5,))
+save_thread.start()
+
+atexit.register(save)
+##
+
+client = discord.Client()
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=message_help.replace('`',''))) # add what activity the bot make
 
 @client.event
 async def on_message(message):
+
     if message.content.startswith(faq_prefix):
         if len(message.content) > len(faq_prefix):
             if message.content[len(faq_prefix)] == ' ': #check the space after the !faq => to modify
+
                 faq_command = message.content[len(faq_prefix)+1:]
                 server_id = str(message.guild.id)
+
                 if faq_command.startswith('list'):
                     if server_id in data:
                         server_faq = data[server_id]['faq']
@@ -60,7 +74,7 @@ async def on_message(message):
                         else:
                             faq_list_text = 'Server\'s FAQs :\n'
                             for faq_name in server_faq:
-                                faq_list_text += '\t' + faq_name + ' : ' + server_faq[faq_name] + '\n'
+                                faq_list_text += '\t' + faq_name + ' : `' + server_faq[faq_name] + '`\n'
                             await message.channel.send(faq_list_text)
                     else:
                         await message.channel.send(message_faq_not_created)
@@ -75,6 +89,7 @@ async def on_message(message):
                     else:
                         name_to_add = faq_command[len('add '):name_end] #TODO ff correct this
                         server_faq[name_to_add] = faq_command[name_end:]
+                        await message.add_reaction('✅') # adding ✅ when added
 
                 elif faq_command.startswith('delete'):
                     if server_id in data:
@@ -82,16 +97,27 @@ async def on_message(message):
                         name_to_delete = faq_command[len('delete '):] #TODO ff correct this
                         if name_to_delete in server_faq:
                             del server_faq[name_to_delete]
-                            await message.channel.send('Successfully deleted')
+                            await message.add_reaction('✅') # adding ✅ when deleted
                         else:
                             await message.channel.send(message_faq_not_found)
                     else:
                         await message.channel.send(message_faq_not_created)
 
-                elif faq_command.startswith('show'):
+                # adding clear command for admins
+                elif faq_command == ('clear all'):
+                    if message.author.guild_permissions.administrator:
+                        data[server_id]['faq'] = {} # empty server faq
+                        await message.channel.send('Successfully cleared ✅')
+                    else:
+                        message.add_reaction('❌') # adding ❌ when the user doesn't have the rights
+                else:
+                    ''' can be problematic if an faq starts with show, delete, add or list or if is clear all :/
+                    if faq_command.startswith('show'):
+                        faq_command = faq_command[len('show '):]
+                    '''
                     if server_id in data:
                         server_faq = data[server_id]['faq']
-                        name_to_show = faq_command[len('show '):] #TODO ff correct this
+                        name_to_show = faq_command
                         if name_to_show in server_faq:
                             await message.channel.send(name_to_show + ' : ' + server_faq[name_to_show])
                         else:
@@ -102,3 +128,5 @@ async def on_message(message):
             await message.channel.send(message_help)
 
 client.run(TOKEN)
+print("stoping")
+save_interval_running = False
